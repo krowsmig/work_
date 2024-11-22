3d72bfd36d1e431c8a219480c43a3d76f597e33e
krowsmig <186128055+krowsmig@users.noreply.github.com>
2024-11-22 03:14:36 -0800

    Update sub_script.js

--- js/sub_script.js
+++ js/sub_script.js
@@ -7,5 +7,33 @@ $(document).ready(function(){
 		return false;
 	});
 
-		
+	
+
+	var getHeights = $("#main").outerHeight() -300;
+	$(window).resize(function(){ 
+		getHeights = $("#main").outerHeight() -300;
+	});
+	$(function(){
+		$(window).scroll(function(){
+			if ($(this).scrollTop() > getHeights) {
+
+				$(".target_ul").addClass("chart_ani");
+
+				setTimeout(function(){
+					$(".target_ul").addClass("after");
+				}, 4000);
+			} else {
+				// $(".target_ul").removeClass("chart_ani");
+				// $(".target_ul").removeClass("after");
+			}
+		}).resize(function(){ 
+			if ($(this).width() < 1024) {
+				
+			} else {
+
+			}
+		});
+	});
+
+
 });
